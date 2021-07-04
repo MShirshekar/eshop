61e701f8db4b2ce8a7e23d3c606e32ec2d12ba42
moeinnoghani <moeinnoghani@yahoo.com>
2021-07-04 17:18:03 +0430

    add some new admin files

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>All rights reserved</b>
+    </div>
+    <strong>online shoppin for webprogramming project</strong>
+</footer>
