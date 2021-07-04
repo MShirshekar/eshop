7ca9bdca00f1ca5e8cf2184c23abefc036ef100c
Mahdi Pakravan <mahdipakravan1998@gmail.com>
2021-07-04 23:25:10 +0430

    modified

--- admin/includes/menubar.php
+++ admin/includes/menubar.php
@@ -8,16 +8,16 @@
       </div>
       <div class="pull-left info">
         <p><?php echo $admin['firstname'].' '.$admin['lastname']; ?></p>
-        <a><i class="fa fa-circle text-success"></i> Online</a>
+        <a><i class="fa fa-circle text-success"></i> آنلاین</a>
       </div>
     </div>
     <!-- sidebar menu: : style can be found in sidebar.less -->
     <ul class="sidebar-menu" data-widget="tree">
-      <li class="header">REPORTS</li>
-      <li><a href="home.php"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a></li>
-      <li><a href="sales.php"><i class="fa fa-money"></i> <span>Sales</span></a></li>
-      <li class="header">MANAGE</li>
-      <li><a href="users.php"><i class="fa fa-users"></i> <span>Users</span></a></li>
+      <li class="header">گزارش‌ها</li>
+      <li><a href="home.php"><i class="fa fa-dashboard"></i> <span>داشبورد</span></a></li>
+      <li><a href="sales.php"><i class="fa fa-money"></i> <span>فروش</span></a></li>
+      <li class="header">مدیریت</li>
+      <li><a href="users.php"><i class="fa fa-users"></i> <span>کاربران</span></a></li>
       <li class="treeview">
         <a href="#">
           <i class="fa fa-barcode"></i>
@@ -27,8 +27,8 @@
           </span>
         </a>
         <ul class="treeview-menu">
-          <li><a href="products.php"><i class="fa fa-circle-o"></i> Product List</a></li>
-          <li><a href="category.php"><i class="fa fa-circle-o"></i> Category</a></li>
+          <li><a href="products.php"><i class="fa fa-circle-o"></i> لیست محصولات</a></li>
+          <li><a href="category.php"><i class="fa fa-circle-o"></i> دسته‌بندی</a></li>
         </ul>
       </li>
     </ul>
