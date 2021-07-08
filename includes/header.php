dd7558f982d4d437078d44122a435b9b037ea92b
Ali Farabi <farabiali.af@gmail.com>
2021-07-08 11:03:53 +0430

    bootstrap rtl has added to project

--- includes/header.php
+++ includes/header.php
@@ -8,6 +8,8 @@
   	<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   	<!-- Bootstrap 3.3.7 -->
   	<link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+  	<!-- Load Bootstrap RTL theme from RawGit -->
+    <link rel="stylesheet" href="//cdn.rawgit.com/morteza/bootstrap-rtl/v3.3.4/dist/css/bootstrap-rtl.min.css">
   	<!-- DataTables -->
     <link rel="stylesheet" href="bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
   	<!-- Font Awesome -->
